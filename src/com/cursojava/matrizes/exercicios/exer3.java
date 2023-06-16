d4c7ceb7b31a47879ee45517e251c32d51a96cd4
distvision <furede2@gmail.com>
2023-06-16 04:00:44 +0200

    more exerciciossss

--- src/com/cursojava/matrizes/exercicios/exer3.java
+++ src/com/cursojava/matrizes/exercicios/exer3.java
@@ -1,5 +1,7 @@
 package com.cursojava.matrizes.exercicios;
 
+import java.util.Scanner;
+
 public class exer3 {
   public static void main(String[] args) {
     /**
@@ -7,5 +9,40 @@ public class exer3 {
      * Após a capture imprima a matriz criada e encontre a quantidade de números
      * pares, a quantidade de números ímpares.
      */
+
+    Scanner sc = new Scanner(System.in);
+
+    int[][] matrizM = new int[3][3];
+
+    for (int i = 0; i < matrizM.length; i++) {
+      for (int j = 0; j < matrizM[i].length; j++) {
+        System.out.print("Digite o valor da posicao [" + i + "," + j + "]");
+        matrizM[i][j] = sc.nextInt();
+      }
+    }
+
+    int qtdPares = 0;
+    int qtdImpares = 0;
+    for (int i = 0; i < matrizM.length; i++) {
+      for (int j = 0; j < matrizM[i].length; j++) {
+        if (matrizM[i][j] % 2 == 0) {
+          qtdPares++;
+        } // podemos usar apenas o else {qtdImpares++}
+        if (matrizM[i][j] % 2 != 0) {
+          qtdImpares++;
+        }
+      }
+    }
+
+    for (int i = 0; i < matrizM.length; i++) {
+      for (int j = 0; j < matrizM[i].length; j++) {
+        System.out.print(matrizM[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+    System.out.println("Os Parese: " + qtdPares);
+    System.out.println("Os Imparese: " + qtdImpares);
+    sc.close();
   }
 }
--- src/com/cursojava/matrizes/exercicios/exer4.java
+++ src/com/cursojava/matrizes/exercicios/exer4.java
@@ -1,16 +1,98 @@
 package com.cursojava.matrizes.exercicios;
 
+import java.util.Scanner;
+
 public class exer4 {
   public static void main(String[] args) {
     /**
-     * Faça um programa para armazenar em uma matriz os compromissos de uma agenda
-     * pessoal.
+     * Faça um programa para armazenar em uma matriz os compromissos de
+     * agenda pessoal.
      * cada dia do mês deve conter 24 horas, onde para cada uma destas 24 horas
      * podemos associar um tarefa especifica (compromisso agendado).
      * O programa deve ter um menu onde o usuario indica o dia mês que deseja
-     * alterar e a hora, entrando em seguida com o compromisso, ou entao, o usuario
-     * pode também consultar a agenda, fornecendo o dia e a hora para obter o
-     * compromisso armazenado.
+     * alterar e a hora, entrando em seguida com o compromisso, ou entao, o
+     * usuario pode também consultar a agenda, fornecendo o dia e a hora
+     * obter o compromisso armazenado.
      */
+
+    Scanner sc = new Scanner(System.in);
+
+    String[][] compromisso = new String[31][24];
+
+    boolean sair = false;
+    byte opcao;
+    while (!sair) {
+      System.out.println("Digite 1 para adicionar compromisso");
+      System.out.println("Digite 2 para verificar compromisso");
+      System.out.println("Digite 0 para sair");
+
+      opcao = sc.nextByte();
+
+      if (opcao == 1) { // Adicionar compromisso
+        boolean diaValido = false;
+        int dia = 0;
+        while (!diaValido) {
+          System.out.println("Digite o dia do mês");
+          dia = sc.nextInt();
+          if (dia > 0 && dia <= 31) {
+            diaValido = true;
+          } else {
+            System.out.println("Dia invalido, digite novamente");
+          }
+        }
+        // hora
+        boolean horaValida = false;
+        int hora = 0;
+        while (!horaValida) {
+          System.out.println("Digite hora do compromisso");
+          hora = sc.nextInt();
+          if (hora >= 0 && hora <= 24) {
+            horaValida = true;
+          } else {
+            System.out.println("hora invalida, digite novamente");
+          }
+        }
+
+        dia--;
+        System.out.println("Digite o compromisso");
+        compromisso[dia][hora] = sc.next();
+
+      } else if (opcao == 2) { // verificar compromisso
+        boolean diaValido = false;
+        int dia = 0;
+        while (!diaValido) {
+          System.out.println("Digite o dia do mês");
+          dia = sc.nextInt();
+          if (dia > 0 && dia <= 31) {
+            diaValido = true;
+          } else {
+            System.out.println("Dia invalido, digite novamente");
+          }
+        }
+        // hora
+        boolean horaValida = false;
+        int hora = 0;
+        while (!horaValida) {
+          System.out.println("Digite hora do compromisso");
+          hora = sc.nextInt();
+          if (hora >= 0 && hora <= 24) {
+            horaValida = true;
+          } else {
+            System.out.println("hora invalida, digite novamente");
+          }
+        }
+
+        dia--;
+        System.out.println("O compromisso agendado é: ");
+        System.out.println(compromisso[dia][hora]);
+
+      } else if (opcao == 0) {
+        sair = true;
+      } else {
+        System.out.println("Opcao invalida, digite novamente");
+      }
+    }
+
+    sc.close();
   }
 }
